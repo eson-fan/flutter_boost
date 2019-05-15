005a29deb884f1aebc043458f515797a65075ec4
esonfan <esonfan@126.com>
2019-05-15 17:24:13 +0800

    修改flutter中closepage时数据回传

--- android/src/main/java/com/taobao/idlefish/flutterboost/NavigationService/NavigationService_closePage.java
+++ android/src/main/java/com/taobao/idlefish/flutterboost/NavigationService/NavigationService_closePage.java
@@ -39,7 +39,7 @@
  
  
         private boolean onCall(MessageResult<Boolean> result,String uniqueId,String pageName,Map params,Boolean animated){
-            FlutterBoostPlugin.containerManager().destroyContainerRecord(pageName,uniqueId,pageName);
+            FlutterBoostPlugin.containerManager().destroyContainerRecord(pageName,uniqueId,params);
             result.success(true);
             return true;
       }
